34876f9aadb660ae3444ab4caba900401f6fb645
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-04 11:57:36 -0500

    feat: post model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { minimum: 5, maximum: 150 }
-  validates :subtitle, length: { minimum: 5, maximum: 50 }
-  validates :url, length: { minimum: 3, maximum: 30 }
+  validates :subtitle, length: { minimum: 5, maximum: 50 }, allow_nil: true
+  validates :url, length: { minimum: 3, maximum: 30 },
+            format: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_nil: true
 end
