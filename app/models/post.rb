fcb594143ebd2453254676c9958b0feeff0a2f99
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-04 11:28:01 -0500

    feat: post model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 150 }
+  validates :subtitle, length: { minimum: 5, maximum: 50 }
+  validates :url, length: { minimum: 3, maximum: 30 }
 end
