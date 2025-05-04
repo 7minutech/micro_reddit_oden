45c682961d2643169cde55b8731fd85fd94a9092
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-04 13:12:05 -0500

    feat: has many comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: true, length: { minimum: 5, maximum: 150 }
   validates :subtitle, length: { minimum: 5, maximum: 50 }, allow_nil: true
